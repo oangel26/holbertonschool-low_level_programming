44066452af384f13d7fab465c7cc70f455125260
Oscar Angel <oa.angel26@gmail.com>
2021-02-04 01:51:06 +0000

    Prints line follow by new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * Prints "Programming is like building a multilingual puzzle"
  * Return: Always 0 (Success)
- 
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle \n");
+  puts("\"Programming is like building a multilingual puzzle");
+
   return 0;
 }
