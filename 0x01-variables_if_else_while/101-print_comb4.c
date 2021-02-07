2a2b1306d8eb9a06c596ba4d1e4e1dd19645ae3d
Oscar Angel <oa.angel26@gmail.com>
2021-02-07 23:42:51 +0000

    program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,7 +24,7 @@ int main(void)
 				{
 				putchar(x + '0');
 
-			       	putchar(y + '0');
+				putchar(y + '0');
 
 				putchar(z + '0');
 
@@ -40,5 +40,5 @@ int main(void)
 	}
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
