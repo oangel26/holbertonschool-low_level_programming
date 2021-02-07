f724f04f26119bf222173a016124350a23d31bad
Oscar Angel <oa.angel26@gmail.com>
2021-02-07 22:37:58 +0000

    prints all possible different combinations of two digits x < y

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description:  Write a program that prints the numbers from 00 to 99
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+/* your code goes there */
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if (x < y)
+			{
+			putchar(x + '0');
+
+			putchar(y + '0');
+			if (x != 8 || y != 9)
+			{
+				putchar(',');
+
+				putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
