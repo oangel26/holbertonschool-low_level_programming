16f3ba7034e02a7d67734fba99918422d27c6c49
Oscar Angel <oa.angel26@gmail.com>
2021-02-08 00:45:27 +0000

    program that prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description:  program that prints all possible combinations of two two-digit
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+	int w;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+				for (z = 0; z <= 9; z++)
+				{
+					for (w = 0; w <= 9; w++)
+					{
+						if ((x + y) < (z + w) && x <= z)
+						{
+
+							putchar(x + '0');
+
+							putchar(y + '0');
+
+							putchar(' ');
+
+							putchar(z + '0');
+
+							putchar(w + '0');
+
+							if (x != 9 || y != 8 || z != 9 || w != 9)
+							{
+								putchar(',');
+
+								putchar(' ');
+
+							}
+						}
+					}
+				}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
