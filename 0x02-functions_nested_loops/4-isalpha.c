5d857181490f4c1db6c043a17ba74873f1eaffaa
Oscar Angel <oa.angel26@gmail.com>
2021-02-09 19:57:45 +0000

    function that checks for alphabetic character 1 if it si lower or upper, 0 else

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main -  entry point
+ * @c: interger of entry
+ * Description: program prints Holberton
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
