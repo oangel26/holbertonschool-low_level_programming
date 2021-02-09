ac32a58e032a7e714079204c12c69343ab648b07
Oscar Angel <oa.angel26@gmail.com>
2021-02-09 22:31:03 +0000

    function that prints every minute of the day of Jack Baue

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,19 +16,19 @@ void jack_bauer(void)
 	for (x = '0'; x <= '2'; x++)
 	{
 		for (y = '0'; y <= '3'; y++)
+		{
+			for (z = '0'; z <= '5'; z++)
 			{
-				for (z = '0'; z <= '5'; z++)
+				for (w = '0'; w <= '9'; w++)
 				{
-					for (w = '0'; w <= '9'; w++)
-					{
-						_putchar(x);
-						_putchar(y);
-						_putchar(':');
-						_putchar(z);
-						_putchar(w);
-						_putchar('\n');
-					}
+					_putchar(x);
+					_putchar(y);
+					_putchar(':');
+					_putchar(z);
+					_putchar(w);
+					_putchar('\n');
 				}
 			}
+		}
 	}
 }
