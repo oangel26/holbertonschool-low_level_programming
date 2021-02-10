7c4c8060e1fd501d85d825a591f26165a7ccfd91
Oscar Angel <oa.angel26@gmail.com>
2021-02-10 03:27:08 +0000

    function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @x: interger value x
+ * @y: interger value y
+ * Description: function that adds two integers and returns the result
+ * Return: addition of parameters.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
