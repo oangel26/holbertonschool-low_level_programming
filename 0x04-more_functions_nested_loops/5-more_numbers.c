54fada0c8525e4a4abd6ea082815a533a7ffe809
Oscar Angel <oa.angel26@gmail.com>
2021-02-15 10:14:52 +0000

    print numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,19 @@ void more_numbers(void)
 {
         int x;
         int y;
+	int z;
 
-	for (x = 0; x <= 9; x++)
+	for (z = 0; z <= 9; z++)
 	{
-		_putchar('0' + x);
+		for (x = 0; x <= 9; x++)
+		{
+			_putchar('0' + x);
+		}
+		for (y = 0; y <= 4; y++)
+		{
+			_putchar('1');
+			_putchar('0' + y);
+		}
+		_putchar('\n');
 	}
-	for (y = 0; y <= 4; y++)
-	{
-		_putchar('1');
-		_putchar('0' + y);
-	}
-	_putchar('\n');
 }
