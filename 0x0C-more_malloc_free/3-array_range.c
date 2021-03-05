8da7093b4968d645b20d073376d9ce502376beb9
Oscar Angel <oa.angel26@gmail.com>
2021-03-05 02:06:14 +0000

    first commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+int *array_range(int min, int max)
+{
+	int *p;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(int) * (max - min + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+		for (i = 0; i <= (max - min); i++)
+		{
+			p[i] = min + i;
+		}
+	return (p);
+	free(p);
+}
