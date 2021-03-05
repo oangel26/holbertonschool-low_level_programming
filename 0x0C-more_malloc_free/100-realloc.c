0cc788c919e79bd453d40d7f6a2ce27d6d032441
Oscar Angel <oa.angel26@gmail.com>
2021-03-05 02:45:40 +0000

    Style corrected

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *p;
 
-        if (ptr == NULL)
+	if (ptr == NULL)
         {
                 ptr = malloc(new_size);
                 return (ptr);
