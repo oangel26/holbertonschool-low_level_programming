c8cf0e6346d41837a2542bac74a82f6aec62f45d
Oscar Angel <oa.angel26@gmail.com>
2021-03-11 00:24:15 +0000

    First commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: string
+ * @f: pointer to function
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+       	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - exe. a funct. given as a para. on each element of an array
+ * @array: array
+ * @siz: size of the array
+ * @action: pointer to function void return with interger innput
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int  i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		action(array[i]);
+	}
+}
