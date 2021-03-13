f163515a27a900c9371ca7b55cf913067796c6bc
Oscar Angel <oa.angel26@gmail.com>
2021-03-13 05:14:53 +0000

    (nil) condition added to the program if any arg. param is null

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,28 +13,25 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
+	va_list ap2;
 	unsigned int i;
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	va_start(ap, n);
+	va_start(ap2, n);
 	for (i = 0; i < n; i++)
 	{
-		if (ap == NULL)
-		{
-			printf("(nil)%s", separator);
-		}
-		else if (separator == NULL || i == n - 1)
+		if (separator == NULL || i == n - 1)
 		{
 			printf("%s", va_arg(ap, char *));
 		}
-		else
+		else if (va_arg(ap2, char *) != NULL)
 		{
-			printf("%s%s", va_arg(ap, char *), separator);
+			printf("%s%s", va_arg(ap, char *),separator );
 		}
+		else
+			printf("(nil)");
 	}
 	printf("\n");
 	va_end(ap);
+	va_end(ap2);
 }
