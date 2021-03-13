251930c0875a03a8507f207da05c975440c0b1bb
Oscar Angel <oa.angel26@gmail.com>
2021-03-13 03:41:57 +0000

    Added NULL condition paramenter pointer

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,6 +15,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	if (separator == NULL)
+	{
+		return;
+	}
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
