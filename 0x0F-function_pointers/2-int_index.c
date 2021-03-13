c2bc8eb1f9abe7c70b2d4eb75c2635c48f9fde25
Oscar Angel <oa.angel26@gmail.com>
2021-03-13 00:46:58 +0000

    pointer NULL constriction added and return fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,18 +14,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) == 1)
+	else
+		for (i = 0; i < size; i++)
 		{
-			break;
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
 		}
-		else
-			return (-1);
-	}
-	return (i);
+	return (-1);
 }
