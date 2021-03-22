68d8929dbfb657ac08e7368c1d3af899e3fe1d95
Oscar Angel <oa.angel26@gmail.com>
2021-03-22 19:42:06 +0000

    first commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,30 +4,49 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * add_node_end -  function that adds a new node at the end of a list.
+ * @head: pointer to apointer whith the address of the head
+ * @str: constrant string 
+ * Return: pointer to a data structure list (list_t)
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int n = 0;
 	int i;
-	int cont = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		cont++;
+		n++;
 	}
 	if (*head == NULL)
 	{
 		new_node = malloc(sizeof(list_t));
+		if (new_node == NULL)
+		{
+			return (NULL);
+		}
 		new_node->str = strdup(str);
-		new_node->len = cont;
-		new_node->next = new_node;
-		new_node = NULL;
-		
+		new_node->len = n;
+		new_node->next = NULL;
+		*head = new_node;
 	}
-
-	return (new_node);
+	else
+	{
+		new_node = *head;
+		while (new_node->next != NULL)
+		{
+			new_node = new_node->next;
+		}
+		new_node->next = malloc(sizeof(list_t));
+		if (new_node->next == NULL)
+		{
+			return (NULL);
+		}
+		new_node->next->str = strdup(str);
+		new_node->next->len = n;
+		new_node->next->next = NULL;
+	}
+	return (*head);
 }
