04cd4d8536b07361bb188713cdcadc27716c7208
Oscar Angel <oa.angel26@gmail.com>
2021-03-22 15:53:01 +0000

    style and guard conditions corrected

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,17 +23,26 @@ list_t *add_node(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		new_node = malloc(sizeof(list_t));
+		if (new_node == NULL)
+		{
+			return (NULL);
+		}
 		new_node->str = strdup(str);
 		new_node->len = n;
 		new_node->next = NULL;
+		*head = new_node;
 	}
 	else
 	{
 		new_node = malloc(sizeof(list_t));
+		if (new_node == NULL)
+		{
+			return (NULL);
+		}
 		new_node->str = strdup(str);
 		new_node->len = n;
 		new_node->next = *head;
+		*head = new_node;
 	}
-	*head = new_node;
 	return (*head);
 }
