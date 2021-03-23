51f15bd68c579348e70ff1706765558642803551
Oscar Angel <oa.angel26@gmail.com>
2021-03-23 18:00:07 +0000

    Uninitialized value of pointer error corrected

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -27,10 +27,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	else
 	{
+		ptrNode = *head;
 		while (ptrNode->next != NULL)
-		{
-			ptrNode = ptrNode->next;
-		}
+                {
+                        ptrNode = ptrNode->next;
+                }
 		ptrNode->next = malloc(sizeof(listint_t));
 		if (ptrNode->next == NULL)
 		{
