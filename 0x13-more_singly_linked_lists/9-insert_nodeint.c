9a608f6767a0d8dd90d2b254a7448b645f716bc8
Oscar Angel <oa.angel26@gmail.com>
2021-03-24 17:47:19 +0000

    Add node in initial position condition updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,12 +17,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptrNode;
 	listint_t *newNode;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
+		if (idx == 0)
+		{
+			newNode = malloc(sizeof(listint_t));
+			if (newNode == NULL)
+                        {
+                                return (NULL);
+                        }
+			(newNode)->n = n;
+			(newNode)->next = *head;
+			*head = newNode;
+		}
 		ptrNode = *head;
 		while (i < idx - 1)
 		{
@@ -39,10 +50,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			{
 				return (NULL);
 			}
-			newNode->n = n;
-			newNode->next = ptrNode->next;
+			(newNode)->n = n;
+			(newNode)->next = ptrNode->next;
 			ptrNode->next = newNode;
-			free (ptrNode);
 		}
 	}
 	return (newNode);
