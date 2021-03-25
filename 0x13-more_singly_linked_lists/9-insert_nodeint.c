0f3a7c8ade3f787ab0da565c61601446d63748f3
Oscar Angel <oa.angel26@gmail.com>
2021-03-25 03:05:40 +0000

    NULL return condition added

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			(newNode)->next = *head;
 			*head = newNode;
-			return (newNode);
 		}
 		else
 		{
@@ -49,7 +48,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			}
 			(newNode)->next = ptrNode->next;
 			ptrNode->next = newNode;
+			return (newNode);
 		}
-		return (newNode);
 	}
+	return (NULL);
 }
