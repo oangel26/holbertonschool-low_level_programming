135e4b5c97308c07bc5c5c7a2d9c6c01721d35ed
Oscar Angel <oa.angel26@gmail.com>
2021-03-25 04:00:34 +0000

    first commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
