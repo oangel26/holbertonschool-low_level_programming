6b256236099d945084e60933ecf1592852ac67a1
Oscar Angel <oa.angel26@gmail.com>
2021-04-16 23:44:28 +0000

    library udated

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,8 +1,16 @@
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
-int _putchar(char c);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* libs */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* prototypes */
+char *create_array(unsigned int, char);
+char *_strdup(char *);
+int **alloc_grid(int, int);
+char *str_concat(char *, char *);
+void free_grid(int **, int);
+char *argstostr(int, char **);
+
+#endif
