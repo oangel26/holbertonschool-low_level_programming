e3b8a4e1ccc9c37387daf33fab478a6690e75e49
Oscar Angel <oa.angel26@gmail.com>
2021-04-16 23:47:52 +0000

    errors fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,25 +1,48 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
- * _calloc - Entry point
- * @nmemb: unsigned int
- * @size: unsigned int
- * Return: Always (buffer)
+ * str_concat - concatenate two strings and return new string
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to char concatenated
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *str_concat(char *s1, char *s2)
 {
-	char *buffer;
+	char *str;
+	int i = 0, j = 0, len1 = 0, len2 = 0, size = 0;
 
-	int long i;
+	if (!s1 && !s2) /* edge case any s is null*/
+		return (malloc(sizeof(char)));
 
-	if (nmemb == 0 || size == 0)
+	if (s1) /* get length of two strings*/
+	{
+		for (; s1[len1]; len1++)
+		{}
+	}
+
+	if (s2)
+	{
+		for (; s2[len2]; len2++)
+		{}
+	}
+	size = len1 + len2;
+
+	/* reserve this space based of lengths*/
+	str = malloc(sizeof(char) * size + 1);
+
+	if (!str)
 		return (NULL);
-	buffer = malloc((nmemb * size));
-	if (buffer == NULL)
-		return (NULL);
-	for (i = 0; i < (nmemb * size); i += 1)
-		*(buffer + i) = 0;
 
-	return ((void *)buffer);
+	for (; i < size; i++) /* fill new string */
+	{
+		if (i < len1) /* if copy first string */
+			str[i] = s1[i];
+		else /* else fill second s2 in new string*/
+		{
+			str[i] = s2[j];
+			j++;
+		}
+	}
+
+	str[i] = '\0'; /*add end*/
+	return (str);
 }
