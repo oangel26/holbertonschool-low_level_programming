b34a6e05391e80e0c019ccc87f33862588e1547b
Oscar Angel <oa.angel26@gmail.com>
2021-04-16 22:34:09 +0000

    First commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_binary - print a unsigned int in binary format
+ * @binary: pointer to strig
+ * Return: Binary.
+ */
+void print_binary(unsigned long int n)
+{
+	size_t size;
+	if (n < 2)
+	{
+		_putchar(n + '0');
+		return;
+	}
+
+	for (size = 0; ; size++)
+	{
+
+	}
+	printf("%lu", n << 1);
+}
