92bde089c283fe363e3e9d753aa39217836cedca
Oscar Angel <oa.angel26@gmail.com>
2021-04-16 22:20:50 +0000

    libraries added

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,12 +1,26 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/* libraries*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/* constant values */
+#define BUFFER_SIZE 1024
+#define MSG_ERROR_97 "Usage: cp file_from file_to\n"
+#define MSG_ERROR_98 "Error: Can't read from file %s\n"
+#define MSG_ERROR_99 "Error: Can't write to %s\n"
+#define MSG_ERROR_100 "Error: Can't close fd %d\n"
+#define WRITE_READ 0664
+
+
+/* prototypes*/
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-void write_text(int file_d, char *buffer, const char *filename, ssize_t size);
-ssize_t read_text(int file_d, char *buffer, const char *filename);
-void error(int exit_n, const char *filename, int fd);
 
 #endif
