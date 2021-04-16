f8007d8ef5347d34ec1186402bca1b147c3145e7
Oscar Angel <oa.angel26@gmail.com>
2021-04-16 00:24:20 +0000

    Min limit error fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,25 +2,27 @@
 
 /**
  * print_last_digit - function that prints the last digit of a number
- * @x: entry number
+ * @x: int entry number
  * Return: return last digit
  */
 
 int print_last_digit(int x)
 {
-	if (x < 0)
+	long int number = x;
+
+	if (number < 0)
 	{
-		_putchar(((x * -1) % 10) + '0');
-		return ((x * -1) % 10);
+		_putchar(((number * -1) % 10) + '0');
+		return ((number * -1) % 10);
 	}
-	else if (x == 0)
+	else if (number == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar((x % 10) + '0');
-		return (x % 10);
+		_putchar((number % 10) + '0');
+		return (number % 10);
 	}
 }
