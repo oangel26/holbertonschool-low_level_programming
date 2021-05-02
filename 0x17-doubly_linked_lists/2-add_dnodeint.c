96534fb4e2a09d39e5e806d8e983e3e34738735a
Oscar Angel <oa.angel26@gmail.com>
2021-05-02 22:10:47 +0000

    Case of null head added

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,24 +14,37 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	/*guard conditions if head is NULL*/
 	if (*head == NULL)
 	{
-		return (NULL);
+		new = malloc(sizeof(dlistint_t));
+		/*guard condition if not enough memory*/
+		if (new == NULL)
+		{
+			dprintf(2, "Error: Can't malloc\n");
+			return (NULL);
+		}
+		/*new node initializacion and linking*/
+		new->n = n;
+		new->next = NULL;
+		new->prev = NULL;
+		*head = new;
 	}
-        new = malloc(sizeof(dlistint_t));
-	/*guard condition if not enough memory*/
-	if (new == NULL)
+	else
 	{
-		dprintf(2, "Error: Can't malloc\n");
-		return (NULL);
+		new = malloc(sizeof(dlistint_t));
+		/*guard condition if not enough memory*/
+		if (new == NULL)
+		{
+			dprintf(2, "Error: Can't malloc\n");
+			return (NULL);
+		}
+		/*new node initialization and linking*/
+		new->n = n;
+		new->prev = NULL;
+		new->next = *head;
+		if (*head != NULL)
+		{
+			(*head)->prev = new;
+		}
+		*head = new;
+		return (new);
 	}
-
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-
-	if (*head != NULL)
-	{
-		(*head)->prev = new;
-	}
-	*head = new;
-	return (new);
 }
