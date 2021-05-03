19443334e13866ee80d23b626b4a9e5d58e3f437
Oscar Angel <oa.angel26@gmail.com>
2021-05-03 01:26:19 +0000

    Style error corrected

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - function that return the nth of a node in linked list
+ * get_dnodeint_at_index - function that return the nth of a node in list
  * @head: pointer to node data structre
  * @index: unsigned int containing index of node to return
  * Return: Always EXIT_SUCCESS.
