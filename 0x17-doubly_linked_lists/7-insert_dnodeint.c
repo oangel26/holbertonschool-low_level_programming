b97354236186dbaea509abf01d2f25b7759fdd18
Oscar Angel <oa.angel26@gmail.com>
2021-05-03 03:35:54 +0000

    Style corrections

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,48 +12,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
 	dlistint_t *tmp;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	unsigned int i, j = 0;
 
-	/*guard condition for node if NULL*/
 	if (h == NULL)
 	{
 		return (NULL);
 	}
-	/*Loop to find the length of list and create a node*/
 	tmp = *h;
 	while (tmp != NULL)
 	{
-                i++;
-                tmp = tmp->next;
-        }
+		i++;
+		tmp = tmp->next;
+	}
 	new = malloc(sizeof(dlistint_t));
-	/*guard condition if not enough memory*/
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-	/*Condition if node given is added at the beginning*/
 	if (idx == 0)
 	{
+		new->n = n;
+		new->prev = NULL;
 		if (*h == NULL)
 		{
-			new->n = n;
 			new->next = NULL;
-			new->prev = NULL;
-			*h = new;
 		}
 		else
 		{
-			new->n = n;
-			new->prev = NULL;
 			new->next = *h;
 			(*h)->prev = new;
-			*h = new;
 		}
-			return (new);
+		*h = new;
+		return (new);
 	}
-	/*Condition if node given is added at the end*/
 	else if (i == idx)
 	{
 		new->n = n;
@@ -62,10 +53,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp->next = new;
 		return (new);
 	}
-	/*Condition if node given is added in the middle*/
 	else if (idx < i)
 	{
-		/*Loop to reach the index node*/
 		tmp = *h;
 		while (j != idx)
 		{
@@ -75,7 +64,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->n = n;
 		new->next = tmp;
 		new->prev = tmp->prev;
-		tmp->prev->next= new;
+		tmp->prev->next = new;
 		tmp->prev = new;
 		return (new);
 	}
