7db78ec54f7edd2637a8891ee36c2e3ad074802f
Oscar Angel <oa.angel26@gmail.com>
2021-05-03 01:24:39 +0000

    First commit

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function that return the nth of a node in linked list
+ * @head: pointer to node data structre
+ * @index: unsigned int containing index of node to return
+ * Return: Always EXIT_SUCCESS.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	/*guard condition to NULL pointer*/
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (i != index)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
