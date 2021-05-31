7e7979e0623fec507609d59b1b07b0607960fec9
Oscar Angel <oa.angel26@gmail.com>
2021-05-31 00:58:27 +0000

    malloc memory allocation error corrected

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,9 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		perror("malloc cant alloc enough memory");
+	}
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
-
 	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
