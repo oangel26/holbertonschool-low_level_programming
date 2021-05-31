2fb9d7ed63eb450c88027e946b35478f76caa471
Oscar Angel <oa.angel26@gmail.com>
2021-05-31 01:04:25 +0000

    Style error corrected

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
-	
 	if (table->array == NULL)
 	for (i = 0; i < size; i++)
 	{
