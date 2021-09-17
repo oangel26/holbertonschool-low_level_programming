c6aa44bd998ea51d92f9021ef0275a71c46d4ba2
oangel26 <oa.angel26@gmail.com>
2021-09-17 17:55:18 -0500

    Style correction

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -14,15 +14,15 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t num_nodes;
 
 	num_nodes = 0;
-	
+
 	if (h == NULL)
 		return (0);
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		num_nodes++;
 		h = h->next;
 	}
-	return num_nodes;
+	return (num_nodes);
 }
