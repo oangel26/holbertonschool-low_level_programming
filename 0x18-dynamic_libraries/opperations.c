17a33ed9ddcc751b68926df8c1fb976bceb6f7fa
oangel26 <oa.angel26@gmail.com>
2021-09-24 16:58:53 -0700

    first commit

---
+++ 0x18-dynamic_libraries/opperations.c
@@ -0,0 +1,61 @@
+/**
+ * add - Function that adds two intergers
+ * @a: interger a
+ * @b: interger b
+ * Return: integer sum of a and b
+ */
+
+int add(int a, int b)
+{
+  return (a + b);
+}
+
+/**
+ * sub - Function that subtracts two intergers
+ * @a: interger a
+ * @b: interger b
+ * Return: integer subtactrion of a and b
+ */
+
+int sub(int a, int b)
+{
+  return (a - b);
+}
+
+/**
+ * mul - Function that multiplies two intergers
+ * @a: interger a
+ * @b: interger b
+ * Return: integer multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+  return (a * b);
+}
+
+/**
+ * div - Function that divides two intergers
+ * @a: interger a
+ * @b: interger b
+ * Return: integer dividsion of a and b
+ */
+
+int div(int a, int b)
+{
+  return (a / b);
+}
+
+/**
+ * mod - Function that return mod of two intergers
+ * @a: interger a
+ * @b: interger b
+ * Return: integer sum of a and b
+ */
+
+int mod(int a, int b)
+{
+  return (a % b);
+}
+
+
