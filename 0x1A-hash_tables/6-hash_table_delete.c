68799c73c66b01a68a7a5a75982e4e576c0368fd
oangel26 <oa.angel26@gmail.com>
2021-10-11 09:43:03 -0500

    free nodes created

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,6 +8,23 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	hash_node_t *ptr = NULL;
+	hash_node_t *tmp = NULL;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			ptr = ht->array[i];
+			while (ptr != NULL)
+			{
+				tmp = ptr;
+				ptr = ptr->next;
+				free(tmp);
+			}
+		}
+	}
 	free(ht->array);
 	free(ht);
 }
