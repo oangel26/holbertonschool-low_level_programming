981105cc81ae1aa9d1cdd49b0f7d7d1997e5d2c8
oangel26 <oa.angel26@gmail.com>
2021-10-11 08:08:33 -0500

    test

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,44 +8,6 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
-	hash_node_t *tmp;
-	hash_node_t *ptr;
-
-	if (ht == NULL)
-		return;
-
-	printf("paso por return");
-	for (i = 0; i <= ht->size; i++)
-	{
-		if (ht->array[i] == NULL)
-		{
-			printf("%s key ptr befor %p\n", ht->array[i]->key,(void *)ht->array[i]);
-			free(ht->array[i]);
-			continue;
-		}
-		else
-		{
-			if (ht->array[i]->next == NULL)
-			{
-				printf("%s key ptr befor %p\n", ht->array[i]->key,(void *)ht->array[i]);
-				free(ht->array[i]);
-			}
-			else
-			{
-				ptr = ht->array[i];
-				while (ptr != NULL)
-				{
-					tmp = ptr;
-					ptr = ptr->next;
-					printf("%s key ptr befor %p\n", ht->array[i]->key,(void *)ht->array[i]);
-					
-					free(tmp);
-				}
-				free(ptr);
-			}
-		}
-	}
 	free(ht->array);
 	free(ht);
 }
